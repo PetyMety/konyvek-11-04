2e720150d2a3d01a19b3e7e3422114362d2f4fa8
Petrik Diak <diak@petrik.hu>
2024-11-04 13:25:27 +0100

    service

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBookDto } from './dto/create-book.dto';
-import { UpdateBookDto } from './dto/update-book.dto';
 
 @Injectable()
 export class BooksService {
-  create(createBookDto: CreateBookDto) {
-    return 'This action adds a new book';
-  }
+    private books = [];
+    private nextId = 1;
 
-  findAll() {
-    return `This action returns all books`;
-  }
+    findAll() {
+        return this.books;
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} book`;
-  }
+    findOne(id: number) {
+        return this.books.find(book => book.id === id);
+    }
 
-  update(id: number, updateBookDto: UpdateBookDto) {
-    return `This action updates a #${id} book`;
-  }
+    create(createBookDto: CreateBookDto) {
+        const newBook = {
+            id: this.nextId++,
+            ...createBookDto,
+        };
+        this.books.push(newBook);
+        return newBook;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} book`;
-  }
+    delete(id: number) {
+        this.books = this.books.filter(book => book.id !== id);
+    }
+
+    update(id: number, updateBookDto: Partial<CreateBookDto>) {
+        const bookIndex = this.books.findIndex(book => book.id === id);
+        if (bookIndex > -1) {
+            this.books[bookIndex] = { ...this.books[bookIndex], ...updateBookDto };
+            return this.books[bookIndex];
+        }
+        return null;
+    }
 }
